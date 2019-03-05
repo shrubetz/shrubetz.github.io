34c8df406cfdde1a8eae3f6775627aca754ec04e
shrubetz <smhrube@gmail.com>
2019-03-05 14:50:04 -0500

    Update connectfourcanvas.js

--- connectfourcanvas.js
+++ connectfourcanvas.js
@@ -1,5 +1,5 @@
 
-var board [];
+var board [[],[]];
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var colblue=1;
@@ -15,8 +15,8 @@ for(x=0;x<=7;x++){
 	   ctx.arc(x*100+50,y*100+50,40,0,2*Math.PI);
 	   ctx.fill();
        ctx.stroke();
-	  board[x,y]=0;
-	  alert(board[x,y]+"");
+	  board[x][y]=0;
+	  alert(board[x][y]+"");
 	   
   }
 }
